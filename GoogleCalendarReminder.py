ccbab8da11ba76461c8551312e78cfc9500cb9ee
Peter Zhigalov <peter.zhigalov@gmail.com>
2022-04-05 22:53:35 +0700

    Fix autolaunch ability

--- GoogleCalendarReminder.py
+++ GoogleCalendarReminder.py
@@ -7,39 +7,41 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 
 class GoogleCalendarReminder(QtCore.QObject):
-    icon = os.path.join(os.path.dirname(os.path.realpath(__file__)), "GoogleCalendarReminder.svg")
-    update_interval = 2 * 60 * 1000
+    _icon = os.path.join(os.path.dirname(os.path.realpath(__file__)), "GoogleCalendarReminder.svg")
+    _update_interval = 2 * 60 * 1000
 
     def __init__(self):
         super(GoogleCalendarReminder, self).__init__()
-        self.trayIcon = QtWidgets.QSystemTrayIcon()
-        QtWidgets.qApp.aboutToQuit.connect(self.trayIcon.hide)
-        self.trayIcon.setIcon(QtGui.QIcon(self.icon))
-        self.trayIcon.activated.connect(self._on_tray_icon_activated)
+        self._trayIcon = QtWidgets.QSystemTrayIcon()
+        QtWidgets.qApp.aboutToQuit.connect(self._trayIcon.hide)
+        self._trayIcon.setIcon(QtGui.QIcon(self._icon))
+        self._trayIcon.activated.connect(self._on_tray_icon_activated)
 
-        self.trayMenu = QtWidgets.QMenu()
-        self.trayIcon.setContextMenu(self.trayMenu)
-        self.trayMenu.addAction("Open Google Calendar").triggered.connect(self._open_google_calendar)
-        self.trayMenu.addAction("Show Agenda").triggered.connect(self._show_agenda)
-        self.trayMenu.addSeparator()
-        self.trayMenu.addAction("Remind Now").triggered.connect(self._remind_now)
-        autoRemind = self.trayMenu.addAction("Remind Automatically")
+        self._trayMenu = QtWidgets.QMenu()
+        self._trayIcon.setContextMenu(self._trayMenu)
+        self._trayMenu.addAction("Open Google Calendar").triggered.connect(self._open_google_calendar)
+        self._trayMenu.addAction("Show Agenda").triggered.connect(self._show_agenda)
+        self._trayMenu.addSeparator()
+        self._trayMenu.addAction("Remind Now").triggered.connect(self._remind_now)
+        autoRemind = self._trayMenu.addAction("Remind Automatically")
         autoRemind.setCheckable(True)
         autoRemind.setChecked(True)
         autoRemind.triggered.connect(self._on_auto_remind_toggled)
-        self.trayMenu.addSeparator()
-        self.trayMenu.addAction("Exit").triggered.connect(QtWidgets.qApp.quit)
+        self._trayMenu.addSeparator()
+        self._trayMenu.addAction("Exit").triggered.connect(QtWidgets.qApp.quit)
 
-        self.timer = QtCore.QTimer()
-        self.timer.setSingleShot(False)
-        self.timer.setInterval(self.update_interval)
-        self.timer.timeout.connect(self._remind_now)
-        self.timer.start()
+        self._timer = QtCore.QTimer()
+        self._timer.setSingleShot(False)
+        self._timer.setInterval(self._update_interval)
+        self._timer.timeout.connect(self._remind_now)
+        self._timer.start()
 
         QtCore.QTimer.singleShot(1000, self._remind_now)
 
     def show(self):
-        self.trayIcon.show()
+        while not self._trayIcon.geometry().isValid():
+            self._trayIcon.hide()
+            self._trayIcon.show()
 
     def _open_google_calendar(self):
         QtGui.QDesktopServices.openUrl(QtCore.QUrl("https://calendar.google.com"))
@@ -47,22 +49,22 @@ class GoogleCalendarReminder(QtCore.QObject):
     def _show_notification(self, text):
         if not QtCore.QProcess.startDetached("notify-send", [
             "-u", "critical",
-            "-i", self.icon,
+            "-i", self._icon,
             "-a", "GoogleCalendarReminder",
             "Google Calendar Reminder",
             text
         ]):
-            self.trayIcon.showMessage("Google Calendar Reminder", text, self.trayIcon.icon())
+            self._trayIcon.showMessage("Google Calendar Reminder", text, self._trayIcon.icon())
 
     def _update_agenda(self):
         process = QtCore.QProcess()
         process.start("gcalcli", ["--nocolor", "agenda"])
         process.waitForFinished()
-        self.trayIcon.setToolTip(process.readAllStandardOutput().data().decode('utf8'))
+        self._trayIcon.setToolTip(process.readAllStandardOutput().data().decode('utf8'))
 
     def _show_agenda(self):
         self._update_agenda()
-        self._show_notification(self.trayIcon.toolTip())
+        self._show_notification(self._trayIcon.toolTip())
 
     def _remind_now(self):
         process = QtCore.QProcess()
@@ -80,12 +82,12 @@ class GoogleCalendarReminder(QtCore.QObject):
             self._show_agenda()
 
     def _on_auto_remind_toggled(self, checked):
-        if checked and self.timer.isActive():
+        if checked and self._timer.isActive():
             return
         if checked:
-            self.timer.start()
+            self._timer.start()
         else:
-            self.timer.stop()
+            self._timer.stop()
 
 
 def main():
